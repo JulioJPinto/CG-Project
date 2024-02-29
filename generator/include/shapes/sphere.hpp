854af3dfae92d81a8685c55a750b520d7b672684
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-02-29 21:09:37 +0000

    fix: formatting and linting

--- generator/include/shapes/sphere.hpp
+++ generator/include/shapes/sphere.hpp
@@ -1,11 +1,11 @@
 #ifndef SOLAR_SYSTEM_SPHERE_HPP
 #define SOLAR_SYSTEM_SPHERE_HPP
 
-
-#include <vector>
 #include <string>
+#include <vector>
+
 #include "../utils.hpp"
 
 bool generateSphere(float radius, int slices, int stacks, const char* filepath);
 
-#endif //SOLAR_SYSTEM_SPHERE_HPP
+#endif  // SOLAR_SYSTEM_SPHERE_HPP
