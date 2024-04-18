6d617b6566c2b0bc4e629c05683611f879b45cc9
Miguel Gramoso <92408711+gramosomi@users.noreply.github.com>
2024-04-18 10:32:08 +0100

    Mg/genarator phase 3 (#21)
    * patch shape
    
    * patch shape
    
    * tea pot example

---
+++ generator/include/shapes/patches.hpp
@@ -0,0 +1,18 @@
+#ifndef PATCHES_HPP
+#define PATCHES_HPP
+
+#include <string>
+
+#include "../../common/include/utils.hpp"
+
+std::vector<Point> patchTriangles(const char* bezier_patch,
+                                  const int tessellation);
+
+bool generatePatch(const char* bezier_patch, const int tessellation,
+                   const char* fileName);
+
+float bernstein(int i, float t);
+
+Point bezierPatch(const std::vector<Point>& controlPoints, float u, float v);
+
+#endif  // PATCHES_HPP
--- generator/src/main.cpp
+++ generator/src/main.cpp
@@ -4,6 +4,7 @@
 #include "../include/shapes/cone.hpp"
 #include "../include/shapes/cube.hpp"
 #include "../include/shapes/cylinder.hpp"
+#include "../include/shapes/patches.hpp"
 #include "../include/shapes/plane.hpp"
 #include "../include/shapes/sphere.hpp"
 #include "../include/shapes/torus.hpp"
@@ -67,6 +68,16 @@ void generateFigure(int argc, char* argv[]) {
     int slices = std::stoi(argv[4]);
 
     generateCylinder(radius, height, slices, fileName);
+  } else if (figureName == "patch" && argc == 5) {
+    // Generate Patch
+    std::cout << "Generating Patch\n";
+    char* bezier_patch = argv[2];
+    int tessellation = std::stoi(argv[3]);
+    printf("Tessellation: %d\n", tessellation);
+    printf("File name: %s\n", fileName);
+    printf("Bezier patch: %s\n", bezier_patch);
+    generatePatch(bezier_patch, tessellation, fileName);
+
   } else {
     std::cerr << "Invalid arguments\n";
   }
