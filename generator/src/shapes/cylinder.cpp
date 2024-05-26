ec9b288e5b0d61a09df472478295b965d7fdc174
Miguel Gramoso <92408711+gramosomi@users.noreply.github.com>
2024-05-26 20:32:56 +0100

    Mg/cylinder (#38)
    * barrel oil test
    
    * textures cylider
    
    * update cone texture

--- generator/src/shapes/cylinder.cpp
+++ generator/src/shapes/cylinder.cpp
@@ -15,94 +15,119 @@ calculateCylinder(const float radius, const float height, const int slices) {
   std::vector<Point> normals;
   std::vector<Point2D> textures;
 
-  float alpha = 2 * M_PI / slices;
-  float half = height / 2;
+  Point v;
+  Point n;
+  Point2D t;
 
-  float r = 0;
 
-  for (int i = 0; i < slices; i++) {
-    // base
-    float px = radius * sin(r);
-    float pz = radius * cos(r);
-    points.push_back(Point(px, -half, pz));
-    points.push_back(Point(0, -half, 0));
-    r += alpha;
-    px = radius * sin(r);
-    pz = radius * cos(r);
-    points.push_back(Point(px, -half, pz));
+    float delta = 2.0f * M_PI / slices;
 
-    Point n1(0.0f, -1.0f, 0.0f);
-    for (int j = 0; j < 3; j++) {
-      normals.push_back(n1);  // Push normals to normals vector
+    for (int i = 0; i < slices; ++i) {
+        // topo
+        // ponto central
+        n = {0.0f, 1.0f, 0.0f};
+        v = {0.0f, height / 2.0f, 0.0f};
+        t = {0.4375f, 0.1875f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        v = {radius * sin(i * delta), height / 2.0f, radius * cos(i * delta)};
+        t = {0.4375f + 0.1875 * sin(i * delta), 0.1875f + 0.1875 * cos(i * delta)};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        v = {radius * sin((i + 1) * delta), height / 2.0f, radius * cos((i + 1) * delta)};
+        t = {0.4375f + 0.1875 * sin((i + 1) * delta), 0.1875f + 0.1875 * cos((i + 1) * delta)};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+
+        // corpo
+        
+        n = {sin(i + 1 * delta), 0.0f, cos(i + 1 * delta)};
+        v = {radius * sin((i + 1) * delta), height / 2.0f, radius * cos((i + 1) * delta)};
+        t = {(i + 1) / static_cast<float>(slices), 1.0f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {sin(i * delta), 0.0f, cos(i * delta)};
+        v = {radius * sin(i * delta), height / 2.0f, radius * cos(i * delta)};
+        t = {i / static_cast<float>(slices), 1.0f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {sin(i * delta), 0.0f, cos(i * delta)};
+        v = {radius * sin(i * delta), -height / 2.0f, radius * cos(i * delta)};
+        t = {i / static_cast<float>(slices), 0.375f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {sin(i + 1 * delta), 0.0f, cos(i + 1 * delta)};
+        v = {radius * sin((i + 1) * delta), -height / 2.0f, radius * cos((i + 1) * delta)};
+        t = {(i + 1) / static_cast<float>(slices), 0.375f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {sin(i + 1 * delta), 0.0f, cos(i + 1 * delta)};
+        v = {radius * sin((i + 1) * delta), height / 2.0f, radius * cos((i + 1) * delta)};
+        t = {(i + 1) / static_cast<float>(slices), 1.0f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {sin(i * delta), 0.0f, cos(i * delta)};
+        v = {radius * sin(i * delta), -height / 2.0f, radius * cos(i * delta)};
+        t = {i / static_cast<float>(slices), 0.375f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+
+        // base
+
+        n = {0.0f, -1.0f, 0.0f};
+        v = {0.0f, -height / 2.0f, 0.0f};
+        t = {0.8125f, 0.1875f};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {0.0f, -1.0f, 0.0f};
+        v = {radius * sin((i + 1) * delta), -height / 2.0f, radius * cos((i + 1) * delta)};
+        t = {0.8125f + 0.1875 * sin((i + 1) * delta), 0.1875f + 0.1875 * cos((i + 1) * delta)};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        n = {0.0f, -1.0f, 0.0f};
+        v = {radius * sin(i * delta), -height / 2.0f, radius * cos(i * delta)};
+        t = {0.8125f + 0.1875 * sin(i * delta), 0.1875f + 0.1875 * cos(i * delta)};
+
+        points.push_back(v);
+        normals.push_back(n);
+        textures.push_back(t);
+
+        
+
     }
-
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-
-    // face
-    points.push_back(Point(px, -half, pz));
-    points.push_back(Point(px, half, pz));
-
-    Point n2(px, 0.0f, pz);
-    for (int j = 0; j < 2; j++) {
-      normals.push_back(n2);  // Push normals to normals vector
-    }
-
-    r -= alpha;
-    px = radius * sin(r);
-    pz = radius * cos(r);
-    points.push_back(Point(px, half, pz));
-
-    n2 = Point(px, 0.0f, pz);
-    normals.push_back(n2);  // Push normals to normals vector
-
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-
-    points.push_back(Point(px, half, pz));
-    points.push_back(Point(px, -half, pz));
-
-    Point n3 = Point(px, 0.0f, pz);
-
-    for (int j = 0; j < 2; j++) {
-      normals.push_back(n3);  // Push normals to normals vector
-    }
-
-    r += alpha;
-    px = radius * sin(r);
-    pz = radius * cos(r);
-    points.push_back(Point(px, -half, pz));
-
-    n3 = Point(px, 0.0f, pz);
-
-    normals.push_back(n3);  // Push normals to normals vector
-
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-
-    // top
-    r += alpha;
-    px = radius * sin(r);
-    pz = radius * cos(r);
-    points.push_back(Point(px, half, pz));
-    points.push_back(Point(0, half, 0));
-    r -= alpha;
-    px = radius * sin(r);
-    pz = radius * cos(r);
-    points.push_back(Point(px, half, pz));
-
-    Point n4(0.0f, 1.0f, 0.0f);
-    for (int j = 0; j < 3; j++) {
-      normals.push_back(n4);  // Push normals to normals vector
-    }
-
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-    textures.push_back(Point2D(0.5f, 0.5f));
-  }
 
   return std::make_pair(std::make_pair(points, normals), textures);
 }
