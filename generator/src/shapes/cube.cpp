bd2d160f7d094b71c695a0329e478ae00a5e79c3
Mário Rodrigues <mjsrodrigues4@gmail.com>
2024-05-26 11:52:10 +0100

    feat: cube

--- generator/src/shapes/cube.cpp
+++ generator/src/shapes/cube.cpp
@@ -5,153 +5,153 @@
 #include "save3d.hpp"
 #include "utils.hpp"
 
-std::pair<std::pair<std::vector<Point>, std::vector<Point>>,
-          std::vector<Point2D>>
-cubeAllPoints(float length, int divisions) {
-  float halfSize = length / 2.0f;
-  float step = length / divisions;
+std::pair<std::pair<std::vector<Point>, std::vector<Point>>, std::vector<Point2D>> cubeAllPoints(float length, int divisions) {
+    float halfSize = length / 2.0f;
+    float step = length / divisions;
 
-  std::vector<Point> points;
-  std::vector<Point> normals;
-  std::vector<Point2D> textures;
+    std::vector<Point> points;
+    std::vector<Point> normals;
+    std::vector<Point2D> textures;
 
-  for (int i = 0; i < divisions; ++i) {
-    for (int j = 0; j < divisions; ++j) {
-      float v1 = -halfSize + i * step;
-      float u1 = -halfSize + j * step;
-      float v2 = v1 + step;
-      float u2 = u1 + step;
+    for (int i = 0; i < divisions; ++i) {
+        for (int j = 0; j < divisions; ++j) {
+            float v1 = -halfSize + i * step;
+            float u1 = -halfSize + j * step;
+            float v2 = v1 + step;
+            float u2 = u1 + step;
 
-      // Front Face
-      points.push_back(Point(v1, u1, halfSize));
-      points.push_back(Point(v2, u1, halfSize));
-      points.push_back(Point(v1, u2, halfSize));
+            // Front Face
+            points.push_back(Point(v1, u1, halfSize));
+            points.push_back(Point(v2, u1, halfSize));
+            points.push_back(Point(v1, u2, halfSize));
 
-      points.push_back(Point(v1, u2, halfSize));
-      points.push_back(Point(v2, u1, halfSize));
-      points.push_back(Point(v2, u2, halfSize));
+            points.push_back(Point(v1, u2, halfSize));
+            points.push_back(Point(v2, u1, halfSize));
+            points.push_back(Point(v2, u2, halfSize));
 
-      Point n1 = Point(0.0f, 0.0f, 1.0f);
-      for (int i = 0; i < 6; i++) {
-        normals.push_back(n1);
-      }
+            Point n1 = Point(0.0f, 0.0f, 1.0f);
+            for (int k = 0; k < 6; k++) {
+                normals.push_back(n1);
+            }
 
-      textures.push_back(Point2D(u1 / length, v1 / length));
-      textures.push_back(Point2D(u2 / length, v1 / length));
-      textures.push_back(Point2D(u1 / length, v2 / length));
-      textures.push_back(Point2D(u1 / length, v2 / length));
-      textures.push_back(Point2D(u2 / length, v1 / length));
-      textures.push_back(Point2D(u2 / length, v2 / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u2 + halfSize) / length));
 
-      // Back Face
-      points.push_back(Point(v1, u1, -halfSize));
-      points.push_back(Point(v1, u2, -halfSize));
-      points.push_back(Point(v2, u1, -halfSize));
+            // Back Face
+            points.push_back(Point(v1, u1, -halfSize));
+            points.push_back(Point(v1, u2, -halfSize));
+            points.push_back(Point(v2, u1, -halfSize));
 
-      points.push_back(Point(v1, u2, -halfSize));
-      points.push_back(Point(v2, u2, -halfSize));
-      points.push_back(Point(v2, u1, -halfSize));
+            points.push_back(Point(v1, u2, -halfSize));
+            points.push_back(Point(v2, u2, -halfSize));
+            points.push_back(Point(v2, u1, -halfSize));
 
-      Point n2 = Point(0.0f, 0.0f, -1.0f);
-      for (int i = 0; i < 6; i++) {
-        normals.push_back(n2);
-      }
+            Point n2 = Point(0.0f, 0.0f, -1.0f);
+            for (int k = 0; k < 6; k++) {
+                normals.push_back(n2);
+            }
 
-      textures.push_back(Point2D(1.0f - u1 / length, v1 / length));
-      textures.push_back(Point2D(1.0f - u1 / length, v2 / length));
-      textures.push_back(Point2D(1.0f - u2 / length, v1 / length));
-      textures.push_back(Point2D(1.0f - u1 / length, v2 / length));
-      textures.push_back(Point2D(1.0f - u2 / length, v2 / length));
-      textures.push_back(Point2D(1.0f - u2 / length, v1 / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
 
-      // Left Face
-      points.push_back(Point(-halfSize, v1, u1));
-      points.push_back(Point(-halfSize, v1, u2));
-      points.push_back(Point(-halfSize, v2, u1));
+            // Left Face
+            points.push_back(Point(-halfSize, v1, u1));
+            points.push_back(Point(-halfSize, v1, u2));
+            points.push_back(Point(-halfSize, v2, u1));
 
-      points.push_back(Point(-halfSize, v1, u2));
-      points.push_back(Point(-halfSize, v2, u2));
-      points.push_back(Point(-halfSize, v2, u1));
+            points.push_back(Point(-halfSize, v1, u2));
+            points.push_back(Point(-halfSize, v2, u2));
+            points.push_back(Point(-halfSize, v2, u1));
 
-      Point n3 = Point(-1.0f, 0.0f, 0.0f);
-      for (int i = 0; i < 6; i++) {
-        normals.push_back(n3);
-      }
+            Point n3 = Point(-1.0f, 0.0f, 0.0f);
+            for (int k = 0; k < 6; k++) {
+                normals.push_back(n3);
+            }
 
-      textures.push_back(Point2D(v1 / length, 1.0f - u1 / length));
-      textures.push_back(Point2D(v2 / length, 1.0f - u1 / length));
-      textures.push_back(Point2D(v1 / length, 1.0f - u2 / length));
-      textures.push_back(Point2D(v1 / length, 1.0f - u2 / length));
-      textures.push_back(Point2D(v2 / length, 1.0f - u1 / length));
-      textures.push_back(Point2D(v2 / length, 1.0f - u2 / length));
+            textures.push_back(Point2D((i + 0.0f) / divisions, (j + 0.0f) / divisions));
+            textures.push_back(Point2D((i + 1.0f) / divisions, (j + 0.0f) / divisions));
+            textures.push_back(Point2D((i + 0.0f) / divisions, (j + 1.0f) / divisions));
+            textures.push_back(Point2D((i + 1.0f) / divisions, (j + 0.0f) / divisions));
+            textures.push_back(Point2D((i + 1.0f) / divisions, (j + 1.0f) / divisions));
+            textures.push_back(Point2D((i + 0.0f) / divisions, (j + 1.0f) / divisions));
 
-      // Right Face
-      points.push_back(Point(halfSize, v1, u1));
-      points.push_back(Point(halfSize, v2, u1));
-      points.push_back(Point(halfSize, v1, u2));
 
-      points.push_back(Point(halfSize, v1, u2));
-      points.push_back(Point(halfSize, v2, u1));
-      points.push_back(Point(halfSize, v2, u2));
+            // Right Face
+            points.push_back(Point(halfSize, v1, u1));
+            points.push_back(Point(halfSize, v2, u1));
+            points.push_back(Point(halfSize, v1, u2));
 
-      Point n4 = Point(1.0f, 0.0f, 0.0f);
-      for (int i = 0; i < 6; i++) {
-        normals.push_back(n4);
-      }
+            points.push_back(Point(halfSize, v1, u2));
+            points.push_back(Point(halfSize, v2, u1));
+            points.push_back(Point(halfSize, v2, u2));
 
-      textures.push_back(Point2D(v1 / length, u1 / length));
-      textures.push_back(Point2D(v2 / length, u1 / length));
-      textures.push_back(Point2D(v1 / length, u2 / length));
-      textures.push_back(Point2D(v1 / length, u2 / length));
-      textures.push_back(Point2D(v2 / length, u1 / length));
-      textures.push_back(Point2D(v2 / length, u2 / length));
+            Point n4 = Point(1.0f, 0.0f, 0.0f);
+            for (int k = 0; k < 6; k++) {
+                normals.push_back(n4);
+            }
 
-      // Top Face
-      points.push_back(Point(v1, halfSize, u1));
-      points.push_back(Point(v1, halfSize, u2));
-      points.push_back(Point(v2, halfSize, u1));
+            textures.push_back(Point2D((u1 + halfSize) / length, (v1 + halfSize) / length));
+            textures.push_back(Point2D((u2 + halfSize) / length, (v1 + halfSize) / length));
+            textures.push_back(Point2D((u1 + halfSize) / length, (v2 + halfSize) / length));
+            textures.push_back(Point2D((u1 + halfSize) / length, (v2 + halfSize) / length));
+            textures.push_back(Point2D((u2 + halfSize) / length, (v1 + halfSize) / length));
+            textures.push_back(Point2D((u2 + halfSize) / length, (v2 + halfSize) / length));
 
-      points.push_back(Point(v1, halfSize, u2));
-      points.push_back(Point(v2, halfSize, u2));
-      points.push_back(Point(v2, halfSize, u1));
+            // Top Face
+            points.push_back(Point(v1, halfSize, u1));
+            points.push_back(Point(v1, halfSize, u2));
+            points.push_back(Point(v2, halfSize, u1));
 
-      Point n5 = Point(0.0f, 1.0f, 0.0f);
-      for (int i = 0; i < 6; i++) {
-        normals.push_back(n5);
-      }
+            points.push_back(Point(v1, halfSize, u2));
+            points.push_back(Point(v2, halfSize, u2));
+            points.push_back(Point(v2, halfSize, u1));
 
-      textures.push_back(Point2D(u1 / length, 1.0f - v1 / length));
-      textures.push_back(Point2D(u2 / length, 1.0f - v1 / length));
-      textures.push_back(Point2D(u1 / length, 1.0f - v2 / length));
-      textures.push_back(Point2D(u1 / length, 1.0f - v2 / length));
-      textures.push_back(Point2D(u2 / length, 1.0f - v1 / length));
-      textures.push_back(Point2D(u2 / length, 1.0f - v2 / length));
+            Point n5 = Point(0.0f, 1.0f, 0.0f);
+            for (int k = 0; k < 6; k++) {
+                normals.push_back(n5);
+            }
 
-      // Bottom Face
-      points.push_back(Point(v1, -halfSize, u1));
-      points.push_back(Point(v2, -halfSize, u1));
-      points.push_back(Point(v1, -halfSize, u2));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
 
-      points.push_back(Point(v1, -halfSize, u2));
-      points.push_back(Point(v2, -halfSize, u1));
-      points.push_back(Point(v2, -halfSize, u2));
+            // Bottom Face
+            points.push_back(Point(v1, -halfSize, u1));
+            points.push_back(Point(v2, -halfSize, u1));
+            points.push_back(Point(v1, -halfSize, u2));
 
-      Point n6 = Point(0.0f, -1.0f, 0.0f);
-      for (int i = 0; i < 6; i++) {
-        normals.push_back(n6);
-      }
+            points.push_back(Point(v1, -halfSize, u2));
+            points.push_back(Point(v2, -halfSize, u1));
+            points.push_back(Point(v2, -halfSize, u2));
 
-      textures.push_back(Point2D(u1 / length, v1 / length));
-      textures.push_back(Point2D(u2 / length, v1 / length));
-      textures.push_back(Point2D(u1 / length, v2 / length));
-      textures.push_back(Point2D(u1 / length, v2 / length));
-      textures.push_back(Point2D(u2 / length, v1 / length));
-      textures.push_back(Point2D(u2 / length, v2 / length));
+            Point n6 = Point(0.0f, -1.0f, 0.0f);
+            for (int k = 0; k < 6; k++) {
+                normals.push_back(n6);
+            }
+
+            textures.push_back(Point2D((v1 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v1 + halfSize) / length, (u2 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u1 + halfSize) / length));
+            textures.push_back(Point2D((v2 + halfSize) / length, (u2 + halfSize) / length));
+        }
     }
-  }
 
-  return std::pair(std::pair(points, normals), textures);
+    return std::pair(std::pair(points, normals), textures);
 }
+
 
 bool generateCube(float length, int divisions, const char* filepath,
                   bool advanced) {
@@ -160,6 +160,7 @@ bool generateCube(float length, int divisions, const char* filepath,
       cube = cubeAllPoints(length, divisions);
 
   if (advanced) {
+    printf("Saving advanced cube to %s\n", filepath);
     save3DAdvancedfile(cube.first.first, cube.first.second, cube.second,
                        filepath);
   } else {
